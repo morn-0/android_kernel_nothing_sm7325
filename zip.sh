3795bd78d838d47cdc5b9fa0c40149fc7bc10ced
morn-0 <90960002+morn-0@users.noreply.github.com>
2022-09-10 15:30:59 +0800

    Update Packing Script

--- zip.sh
+++ zip.sh
@@ -15,20 +15,13 @@ if [ -f "$kernel" ] && [ -d "$dts_dir" ]; then
 	echo -e "\nKernel compiled succesfully! Zipping up...\n"
 	if [ -d "$AK3_DIR" ]; then
 		cp -r $AK3_DIR AnyKernel3
-		git checkout spacewar &> /dev/null
-	elif ! git clone -q https://github.com/BladeRunner-A2C/AnyKernel3 -b spacewar; then
+	elif ! git clone https://github.com/morn-0/AnyKernel3.git; then
 		echo -e "\nAnyKernel3 repo not found locally and couldn't clone from GitHub! Aborting..."
 		exit 1
 	fi
 	cp $kernel AnyKernel3
 	cat $dts_dir/*.dtb > AnyKernel3/dtb
 	python2 scripts/dtc/libfdt/mkdtboimg.py create AnyKernel3/dtbo.img --page_size=4096 $dts_dir/*.dtbo
-	cp $(find $OUT_DIR/modules/lib/modules/5.4* -name '*.ko') AnyKernel3/modules/vendor/lib/modules/
-	cp $OUT_DIR/modules/lib/modules/5.4*/modules.{alias,dep,softdep} AnyKernel3/modules/vendor/lib/modules
-	cp $OUT_DIR/modules/lib/modules/5.4*/modules.order AnyKernel3/modules/vendor/lib/modules/modules.load
-	sed -i 's/\(kernel\/[^: ]*\/\)\([^: ]*\.ko\)/\/vendor\/lib\/modules\/\2/g' AnyKernel3/modules/vendor/lib/modules/modules.dep
-	sed -i 's/.*\///g' AnyKernel3/modules/vendor/lib/modules/modules.load
-	# rm -rf $OUT_DIR/arch/arm64/boot $OUT_DIR/modules
 	cd AnyKernel3
 	zip -r9 "../$ZIPNAME" * -x .git README.md *placeholder
 	cd ..
